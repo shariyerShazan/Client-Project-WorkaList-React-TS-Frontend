15c1536f4e6a6368cc650eb6b4a9c9dab064466b
shariyerShazan <shariyershazan1@gmail.com>
2025-12-06 01:01:38 +0600

    routes sidebard navbar for dashboard.. and use shadcn ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { RouterProvider } from 'react-router'
+import { Routers } from './routes/Routers'
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-
+      <RouterProvider router={Routers}/>
   </StrictMode>,
 )
